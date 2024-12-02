debf425f0d84e9f741ee5f7e78745ea03a78d956
leli1337 <leli1337@users.noreply.github.com>
2024-12-02 13:39:50 +0000

    Fix styling

--- src/Service/GdprService.php
+++ src/Service/GdprService.php
@@ -6,7 +6,7 @@ use MaxMind\Db\Reader;
 
 class GdprService
 {
-    public STATIC array $EU_COUNTRIES = [
+    public static array $EU_COUNTRIES = [
         'AT' => 'Austria',
         'BE' => 'Belgium',
         'BG' => 'Bulgaria',
@@ -43,42 +43,47 @@ class GdprService
         $ip = self::getUserIp();
         $reader = self::getReader();
         $data = $reader->get($ip);
+
         return $data['country']['is_in_european_union'] ?? false;
 
     }
+
     private static function getUserIp(): string
     {
         foreach (
-            array(
+            [
                 'HTTP_CLIENT_IP',
                 'HTTP_X_FORWARDED_FOR',
                 'HTTP_X_FORWARDED',
                 'HTTP_X_CLUSTER_CLIENT_IP',
                 'HTTP_FORWARDED_FOR',
                 'HTTP_FORWARDED',
-                'REMOTE_ADDR'
-            ) as $key
+                'REMOTE_ADDR',
+            ] as $key
         ) {
             if (array_key_exists($key, $_SERVER) === true) {
                 foreach (explode(',', $_SERVER[$key]) as $ip) {
                     $ip = trim($ip); // just to be safe
                     if (filter_var(
-                            $ip,
-                            FILTER_VALIDATE_IP,
-                            FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE
-                        ) !== false) {
+                        $ip,
+                        FILTER_VALIDATE_IP,
+                        FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE
+                    ) !== false) {
                         return $ip;
                     }
                 }
             }
         }
+
         return $_SERVER['REMOTE_ADDR'];
     }
+
     private static function getReader(): Reader
     {
         if (self::$reader === null) {
             self::$reader = new Reader(config('gdpr-cookie-consent.mmdb_path'));
         }
+
         return self::$reader;
     }
 }
